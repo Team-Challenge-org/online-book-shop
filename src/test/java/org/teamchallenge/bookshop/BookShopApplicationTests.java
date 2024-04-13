b448a4218c300d705a3a75b5739eb0f9a6e18c2e
torbin-viacheslav <torbin.official@gmail.com>
2024-04-13 17:49:19 +0300

    Initial commit

---
+++ src/test/java/org/teamchallenge/bookshop/BookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teamchallenge.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
