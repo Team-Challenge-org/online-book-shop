4b585e79cb0cd0040a569a5a78aa41089ff60efd
viacheslav-torbin <torbin.official@gmail.com>
2024-05-17 17:23:35 +0300

    Added controllers to manage favourite books of user. Added corresponding exception

--- src/main/java/org/teamchallenge/bookshop/dto/BookInCatalogDto.java
+++ src/main/java/org/teamchallenge/bookshop/dto/BookInCatalogDto.java
@@ -1,11 +1,9 @@
 package org.teamchallenge.bookshop.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.teamchallenge.bookshop.enums.Available;
 import org.teamchallenge.bookshop.enums.Category;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 public record BookInCatalogDto(
         long id,
