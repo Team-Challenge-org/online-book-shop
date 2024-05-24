72ba836c0a75d7d761fd4649ec35834c031ef251
viacheslav-torbin <torbin.official@gmail.com>
2024-05-24 14:54:25 +0300

    Uncommented MailConfig.class

--- src/main/java/org/teamchallenge/bookshop/config/MailConfig.java
+++ src/main/java/org/teamchallenge/bookshop/config/MailConfig.java
@@ -1,14 +1,14 @@
-//package org.teamchallenge.bookshop.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.mail.javamail.JavaMailSenderImpl;
-//
-//@Configuration
-//public class MailConfig {
-//    @Bean
-//    public JavaMailSender javaMailSender() {
-//        return new JavaMailSenderImpl();
-//    }
-//}
+package org.teamchallenge.bookshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
