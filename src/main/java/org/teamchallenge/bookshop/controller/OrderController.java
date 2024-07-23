401bcba259fe9e5898eede47316627b0d00ca962
Pavlo Polezhay <pasha200417@gmail.com>
2024-07-23 13:55:49 +0300

    added Update, Delete and FindById methods for Order

--- src/main/java/org/teamchallenge/bookshop/controller/OrderController.java
+++ src/main/java/org/teamchallenge/bookshop/controller/OrderController.java
@@ -5,12 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.teamchallenge.bookshop.dto.OrderDto;
+import org.teamchallenge.bookshop.dto.OrderStatusDto;
 import org.teamchallenge.bookshop.enums.OrderStatus;
 import org.teamchallenge.bookshop.model.Order;
 import org.teamchallenge.bookshop.service.OrderService;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/order")
@@ -27,5 +30,21 @@ public class OrderController {
     public ResponseEntity<List<OrderStatus>> getOrderStatuses() {
         List<OrderStatus> orderStatuses = Arrays.asList(OrderStatus.values());
         return ResponseEntity.ok(orderStatuses);
+    }
+
+    @GetMapping("/findById/{id}")
+    public Optional<Order> getOrderById(@PathVariable Long id) {
+        return orderService.getOrderById(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteOrderById(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Order updateOrderById(@PathVariable Long id, @RequestBody OrderStatusDto orderStatusDto) {
+        Order updatedOrder = orderService.updateOrder(id, orderStatusDto.getOrderStatus(), LocalDateTime.now());
+        return updatedOrder;
     }
 }
